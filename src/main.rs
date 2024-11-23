a7d6a80a4a76b0abf1badbf33ace6d20f04e91b2
ld000 <lidong9144@163.com>
2024-11-23 12:26:35 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use bevy::prelude::PluginGroup;
+use bevy::window::Window;
+use bevy::{app::App, window::WindowPlugin, DefaultPlugins};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Hello Bevy!".to_string(),
+                resolution: (800.0, 600.0).into(),
+                ..Default::default()
+            }),
+            ..Default::default()
+        }))
+        .run();
 }
